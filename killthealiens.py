56a68b866deeb3e34823d39d33562080e1307cf1
Tim Nibert <docz2a@gmail.com>
2016-07-24 16:22:16 +1000

    first commit

---
+++ killthealiens.py
@@ -0,0 +1,338 @@
+#! /usr/bin/env python
+import pygame
+#from pygame.locals import *
+import sys
+import obj
+import random
+
+pygame.init()
+
+#set up window
+screen = pygame.display.set_mode((obj.SCREENW, obj.SCREENH), pygame.DOUBLEBUF)
+pygame.display.set_caption("KILL THE ALIENS")
+
+#set text font
+myfont = pygame.font.SysFont("monospace", 15)
+
+#create singleton images for efficiency
+shipimg = pygame.image.load("spaceshipa.png")
+saucerimg = pygame.image.load("saucera.png")
+bulletimg = pygame.image.load("bullet.png").convert()
+bossimg = pygame.image.load("invader.png")
+
+#actually transparent square
+blacksquare = pygame.Surface((obj.explosion[0].get_width()-15, obj.explosion[0].get_height()-15), pygame.SRCALPHA, 32)
+
+#set up game objects
+ship = obj.Player(shipimg)
+#sprite groups
+saucers = []
+bullets = []
+#gone = False
+#killed = pygame.sprite.Group()
+
+#create enemies
+for x in range(0,3):
+	saucers.append(obj.Enemy(random.randrange(0, obj.SCREENW), random.randrange(0, 100), saucerimg))
+
+#create boss
+boss = obj.Boss(100, -1200, bossimg,0)
+
+#create boss explosions
+boom = []
+boom.append(obj.MoveableObject(0, 0, pygame.Surface((1, 1))))
+
+clock = pygame.time.Clock()
+bg = pygame.image.load("map.png").convert()
+bgoffset = 0
+FPS = 30
+
+#flags
+#0 means play, 1 means user exit, 2 means death, 3 means victory
+endgame = 0
+#0 means no boss, 1 means clear out shop for boss, 2 means boss entering, 
+#3 means boss is out, 4 means dying, 5 means dead
+BEASTMODE = 0
+
+BLACK = (0,0,0)
+blacksquare.fill(BLACK)
+
+score = 0
+time = 0	#total play time
+endtime = 0
+
+changeover = 0	#for scroll change over
+ychng = 0
+
+goright = False
+goleft = False
+
+#print saucers
+deadindex = -10
+
+intro = 1
+introscreen = pygame.image.load("intro.png")
+
+#opening screen
+while(intro == 1):
+	screen.fill(BLACK)
+	screen.blit(introscreen, (0,0))
+	pygame.display.flip()
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			sys.exit()
+		if not hasattr(event, 'key'): continue
+		if event.type == pygame.KEYDOWN:
+			if (event.key == pygame.K_RETURN): intro = 0
+
+
+#begin main game loop
+#this should have all been put in a function T_T
+while(endgame == 0):
+	ticktime = clock.tick(FPS)		#update time in milliseconds
+	time += ticktime
+
+	#add more saucers to increase difficulty as time goes on
+	#maybe we could combine this making number of saucers a function of time
+	#boom, it is done
+	if(len(saucers)-3 < time/12000 and BEASTMODE == 0):
+		#print len(saucers)
+		#print time/6000
+		saucers.append(obj.Enemy(random.randrange(0, obj.SCREENW), random.randrange(-200, -50), saucerimg))
+	#if(time >= 8000 and len(saucers) < 5):
+		#saucers.append(obj.Enemy(random.randrange(0, obj.SCREENW), random.randrange(-200, -50), saucerimg))
+
+	#ENTER THE BOSS
+	if(len(saucers) > 10):		#change that number for max saucers on screen
+		BEASTMODE = 1
+		#del saucers[:]		#this removes the whole list
+
+	#user input
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: endgame = 1
+		if not hasattr(event, 'key'): continue
+		if event.key == pygame.K_ESCAPE: endgame = 1
+		if(event.type == pygame.KEYDOWN):
+			if (event.key == pygame.K_LEFT and ship.x >= 0): 
+				goleft = True
+			elif (event.key == pygame.K_RIGHT and ship.x+ship.width <= obj.SCREENW): 
+				goright = True
+		if(event.type == pygame.KEYUP):
+			if (event.key == pygame.K_LEFT):
+				goleft = False
+			elif (event.key == pygame.K_RIGHT):
+				goright = False
+		if (event.key == pygame.K_SPACE and ship.active): 
+			bullets.append(ship.fire(bulletimg))
+
+	if(goright == True and ship.x+ship.width <= obj.SCREENW): ship.x += 5
+	elif(goleft == True and ship.x >= 0): ship.x -= 5
+
+	ship.updatepos()
+
+	if(BEASTMODE == 3):
+		if(boss.infirerange(ship) > 0):
+			if(random.randrange(0,10) == 1):
+				bullets.append(boss.fire(bulletimg, obj.LEFT))
+			if(random.randrange(0,10) == 1):
+				bullets.append(boss.fire(bulletimg, obj.RIGHT))
+		#elif(random.randrange(0,20) == 1):
+		#	bullets.append(boss.fire(bulletimg, obj.LEFT))
+		#	bullets.append(boss.fire(bulletimg, obj.RIGHT))
+
+	#potentially better collision detection
+	#for bullet in bullets:
+		#bullet.move()
+		#pygame.sprite.spritecollide(bullet, saucers, 1)
+		#if killed: 
+			#print killed
+			#print saucers
+
+	for saucer in saucers:
+		dietest = saucer.move(BEASTMODE)
+		if dietest == 1: 
+			deadindex = saucers.index(saucer)
+			#print "DEAD " + str(deadindex)
+	
+	#print "DEAD " + str(deadindex)
+	#print "LEN " + str(len(saucers))
+
+	#move bullets, check for collisions with player or boss or off screen
+	#explosions as well
+	#just an iteration through all bullets
+	for bullet in bullets:
+		bullet.move()
+		#-60 to go a little off screen, for high up explosions
+		if(bullet.y < -60 or bullet.y > obj.SCREENH): bullet.active = False
+		if(obj.collide(ship, bullet)):
+			ship.health -= 1
+			bullet.active = False
+		elif(BEASTMODE == 3 and obj.collide(boss, bullet)): 
+			boss.health -= 5
+			if(boss.health <= 0): 
+				boss.die()
+				BEASTMODE += 1
+			bullet.active = False
+		if(-1 < bullet.exploding < 4): bullet.explode(time)
+		if(bullet.active == False): bullets.remove(bullet)
+
+	#print "Boss Health: " + str(boss.health)
+	#just for kicks
+	#inefficient collision detection
+	#but it works for now
+	for saucer in saucers:
+		if(obj.collide(saucer, ship)):
+			ship.health -= 1
+			saucer.respawn()
+			#if ship.health <= 0: endgame = 0	#change to 2 for kill
+		for bullet in bullets:
+			if(obj.collide(saucer, bullet)):
+				bullet.x = saucer.x
+				bullet.y = saucer.y
+				bullet.updatepos()
+				#respawn saucer off screen and increment score
+				if(BEASTMODE != 1): saucer.respawn()
+				else: 
+					deadindex = saucers.index(saucer)
+					dietest = 1
+				bullet.explode(time)
+				#saucers.remove(saucer)		#this removes te actual object from the list
+				score += 5		
+
+	#this is so that we don't mess up the previous for iteration
+	if(dietest == 1):
+		saucers.pop(deadindex)
+		dietest = 0
+		#print "LEN " + str(len(saucers))
+		if(len(saucers) == 0): 
+			BEASTMODE = 2
+			boss.inittime = time
+
+	#for player death
+	if(ship.health <= 0): 
+		ship.die()
+	if(ship.active == False):
+		ship.explode(time)
+		if(endtime == 0):
+			endtime = time
+	#for time delay after death
+	if(time >= endtime + 4000 and endtime != 0):
+		#print "game over"
+		endgame = 2
+
+	#if boss got killed make sonic style boss death explosion
+	#explode is called multiple times over several main loops to advance the explosion frame
+	if(BEASTMODE == 4):
+		for splat in boom:
+			#if first pass, initialize explosion sequence
+			if(len(boom) == 1 and splat.exploding == -1):
+				splat.x = random.randrange(boss.x, boss.x+boss.width-obj.explosion[0].get_width()) #subtract explosion width
+				splat.y = random.randrange(boss.y, boss.y+boss.height-obj.explosion[0].get_height())
+				obj.explosion.append(blacksquare)	#to take chunks out of boss
+			splat.explode(time)
+		if(boom[-1].exploding == 2 and len(boom) < 8):
+			boom.append(obj.MoveableObject(random.randrange(boss.x, boss.x+boss.width-obj.explosion[0].get_width()), random.randrange(boss.y, boss.y+boss.height-obj.explosion[0].get_height()), pygame.Surface((1,1))))
+		#if boss is done exploding
+		if(len(boom) == 8 and boom[-1].exploding == len(obj.explosion)-1):
+			BEASTMODE = 5
+			score += 1000
+			endtime = time
+			print "BEASTMODE 5"
+
+	#if(boom[7].exploding == 4):
+	#	endtime = time
+	#	BEASTMODE = 5
+	#meh inefficient
+	#this may have to be reexamined, testing beastmode < 3
+	if(boss.y > 0 and BEASTMODE < 3): BEASTMODE = 3
+
+	#if boss is displayed
+	if(4 > BEASTMODE >= 2):
+		boss.move(ship, time)
+
+	#text rendering
+	healthlbl = myfont.render("Health: " + str(ship.health), 1, (255,255,0))
+	scorelbl = myfont.render("Score: " + str(score), 1, (255,255,0))
+	bosslbl = myfont.render("Boss Health: " + str(boss.health), 1, (255,255,0))	
+	#render images
+	screen.fill(BLACK)
+	
+	if(changeover == 0):
+		screen.blit(bg, (0,0), (0, 2000-obj.SCREENH-bgoffset, obj.SCREENW, 2000-bgoffset))
+	elif(changeover == 1):
+		#print "ychng: " + str(ychng)
+		#print "bgoffset: " + str(bgoffset)
+		screen.blit(bg, (0,0), (0, bg.get_height()-ychng, obj.SCREENW, 2000))
+		screen.blit(bg, (0, ychng), (0, 0, obj.SCREENW, obj.SCREENH - ychng))
+
+	if(BEASTMODE >= 2): screen.blit(boss.image, (boss.x, boss.y))
+	screen.blit(ship.image, (ship.x, ship.y))
+	for saucer in saucers:
+		screen.blit(saucer.image, (saucer.x, saucer.y))
+	for bullet in bullets:
+		screen.blit(bullet.image, (bullet.x, bullet.y))
+	if(BEASTMODE >= 4): 
+		#if(boom[-1].exploding == 2):
+			#print "blacksquare"
+			#boss.image.blit(blacksquare, (boom[-1].x, boom[-1].y))
+		for splat in boom:
+			screen.blit(splat.image, (splat.x, splat.y))
+	#screen.blit(explosion[explframe], (obj.SCREENW/2, obj.SCREENH/2))
+	screen.blit(healthlbl, (obj.SCREENW - 100, 20))
+	screen.blit(scorelbl, (obj.SCREENW - 100, 35))
+	if(BEASTMODE >= 3): screen.blit(bosslbl, (obj.SCREENW/2, 20))
+
+	#for i in range(0,5):
+		#screen.blit(obj.explosion[i], (i*120, 300))
+
+	pygame.display.flip()			#apply double buffer
+
+	#if(explframe > 3): explframe=0
+	#else: explframe += 1
+	
+	#change offset for vertical scroll
+	if(bgoffset > 2000):
+		bgoffset = 0
+		changeover = 0
+		ychng = 0
+	else: bgoffset+=2
+
+	if(2000 >= bgoffset > 2000-obj.SCREENH):
+		ychng += 2
+		changeover = 1
+	#print "time: " + str(time)
+	#print "health: " + str(ship.health)
+	#print "score: " + str(score)
+#end game loop
+
+#display end screens
+if(BEASTMODE == 5):
+	disp = pygame.image.load("victory.png")
+else:
+	disp = pygame.image.load("dead.png")
+
+#add loop to get input, continue to high scores, etc
+cont = 0
+while(cont == 0):
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: cont = 1
+		if not hasattr(event, 'key'): continue
+		if event.key == pygame.K_ESCAPE: cont = 1
+	screen.blit(disp, (0,0))
+	pygame.display.flip()
+#elif(endgame == 1): continue
+
+#update and view high scores
+#open file
+#scores = []
+#f = open('scores', 'rw')
+#for line in f:
+#	scores.append(line)
+#print scores
+#read scores into list
+#compare score to list
+#display high scores
+
+
+pygame.quit()
+sys.exit()
---
+++ obj.py
@@ -0,0 +1,222 @@
+import pygame
+import random
+
+SCREENH = 600
+SCREENW = 640
+LEFT = 0
+RIGHT = 1
+UP = 2
+DOWN = 3
+BLACK = (0,0,0)
+EXTIMELAPSE = 250
+
+#for that singleton efficiency
+#saucerimg = pygame.image.load("saucera.png")
+#bulletimg = pygame.image.load("bullet.png")
+
+def toframes(img, numframes, xstep):
+	#img to divide up, number of frames to generate, step size on x axis to split on
+	frames = []	#list of images
+	for i in range(0, numframes):
+		workimg = pygame.Surface((xstep, img.get_height()), pygame.SRCALPHA, 32)
+		#workimg = workimg.convert_alpha()
+		workimg.blit(img, (0,0), area=pygame.Rect(xstep*i, 0, xstep, img.get_height()))
+		frames.append(workimg.copy())
+	return frames
+
+explosion = toframes(pygame.image.load("explode.png"), 5, 120)
+
+class MoveableObject(pygame.sprite.Sprite):
+	def __init__(self, x, y, img):
+		pygame.sprite.Sprite.__init__(self)
+		self.x = x
+		self.y = y
+		self.image = img
+		self.width = self.image.get_width()
+		self.height = self.image.get_height()
+		self.rect = self.image.get_rect()
+		self.updatepos()
+		self.exploding = -1
+		self.timestack = []
+		self.active = True
+	def updatepos(self):	#this lets us play nice with pygame
+		self.pos = (self.x, self.y)
+		self.rect.x = self.x
+		self.rect.y = self.y
+	def explode(self, time):
+		self.timestack.append(time)
+		self.speed = 0
+		if(len(self.timestack) < 2):
+			difftime = EXTIMELAPSE + 1
+		elif(len(self.timestack) >= 2):
+			newtime = self.timestack.pop()
+			oldtime = self.timestack.pop()
+			difftime = newtime - oldtime
+			#print "newtime: " + str(newtime) + " oldtime: " + str(oldtime) + " diff: " + str(difftime)
+			if(difftime <= EXTIMELAPSE): self.timestack.append(oldtime)
+			else: self.timestack.append(newtime)
+		if(self.exploding < len(explosion)-1 and difftime > EXTIMELAPSE):
+			self.exploding += 1
+			self.image = explosion[self.exploding]
+		if(self.exploding >= len(explosion)-1): 
+			#self.image.fill(BLACK)
+			self.image = explosion[len(explosion)-1]
+			self.active = False
+		#then return to main for cleanup
+		#print self.active
+		#print self.timestack
+
+class Player(MoveableObject):
+	def __init__(self, img):
+		MoveableObject.__init__(self, SCREENW/2, 450, img)
+		self.health = 3
+	def fire(self, img):
+		return Bullet(self.x+(self.image.get_width()/2), self.y-10, img, UP)
+	def die(self):
+		#print "player dead"
+		self.active = False
+
+class Enemy(MoveableObject):
+	def __init__(self, x, y, img):
+		MoveableObject.__init__(self, x, y, img)
+		#0 means left, 1 means right
+		self.dir = random.randrange(0, 2)
+		self.xspeed = random.randrange(1, 4)
+		self.yspeed = random.randrange(1, 4)
+	def move(self, beast):
+		#check direction -- these don't match to the predefines
+		if(self.dir == 0):
+			self.x -= self.xspeed
+			self.y += self.yspeed
+			self.updatepos()
+		else:
+			self.x += self.xspeed
+			self.y += self.yspeed
+			self.updatepos()
+		#check horizontal border
+		if(self.x < 0): self.dir = 1
+		elif(self.x > SCREENW-50): self.dir = 0
+
+		#check going off bottom of screen
+		if(self.y > SCREENH and beast != 1): 
+			self.__init__(random.randrange(0, SCREENW), 0, self.image)
+		elif(self.y > SCREENH and beast == 1):	#do not init again, go cleanly off screen
+			return beast
+
+		self.updatepos()
+		#print self.rect
+		return 0
+	def respawn(self):
+		self.__init__(random.randrange(0, SCREENW), random.randrange(-200,-50), self.image)
+		
+
+class Boss(MoveableObject):
+	def __init__(self, x, y, img, time):
+		MoveableObject.__init__(self, x, y, img)
+		self.health = 2000
+		self.inittime = time
+		#state indicator for boss
+		#0 is still, 1 is aimless moving
+		#2 is chase ship, 3 is fire at ship
+		self.mode = 0
+		#counts number of steps in a given direction
+		self.step = 0
+		self.maxstep = 4
+		self.stepdist = 4
+		#initial direction
+		self.dir = random.randrange(0,4)
+		self.active = True
+		#for mode 2
+		self.alreadygoing = 0
+	def move(self, foe, time):
+		#so this is going to need a little AI
+		#and maybe some smoothing of the movement
+		#and tracking of foe
+		#and discrimination of shooting
+
+		#update mode based on timer
+		if(time-self.inittime > 1000):
+			self.inittime = time
+			if(self.mode < 3): self.mode += 1
+			else: self.mode = 0
+
+		if(self.mode == 0): return
+
+		elif(self.mode == 1):
+			if(self.step < self.maxstep):
+				self.step += 1
+			else:
+				self.dir = random.randrange(0,4)
+				self.step = 0
+				self.maxstep = random.randrange(2,6)
+
+		elif(self.mode == 2):
+			test = self.infirerange(foe)
+			if(test == -3 and self.alreadygoing == 0):		#if ship is in middle
+				self.dir = random.randrange(0,2)
+				self.alreadygoing = 1
+			elif(test == -1 and self.alreadygoing == 0):
+				self.dir = LEFT
+				self.alreadygoing = 1
+			elif(test == -2 and self.alreadygoing == 0):
+				self.dir = RIGHT
+				self.alreadygoing = 1
+			elif(test > 0):
+				self.mode = 0
+				self.alreadygoing = 0
+		
+		#check for screen boundaries
+		if(self.y+self.height > foe.y-60): 
+			self.dir = UP
+			self.step = 0
+		if(self.y < 0): 
+			self.dir = DOWN
+			self.step = 0
+		if(self.x < -50):
+			self.dir = RIGHT
+			self.step = 0
+		if(self.x+self.width-50 > SCREENW):
+			self.dir = LEFT
+			self.step = 0
+
+		#self.dir = random.randrange(2,4)
+		if(self.dir == DOWN):		#move down
+			self.y += self.stepdist
+		elif(self.dir == UP): 		#move up
+			self.y -= self.stepdist
+		elif(self.dir == LEFT):		#move left
+			self.x -= self.stepdist
+		elif(self.dir == RIGHT):		#move right
+			self.x += self.stepdist
+
+		self.updatepos()
+	def fire(self, img, side):
+		if(side == LEFT): return Bullet(self.x, self.y+self.height, img, DOWN)
+		else: return Bullet(self.x+self.width, self.y+self.height, img, DOWN)
+	def infirerange(self, foe):
+		#self.x is left turret, self.x+self.width is right turret
+		#return 1 if left turret, return 2 if right
+		#return -1 if too far left, -2 if too far right, -3 if in center
+		if(foe.x+foe.width > self.x and foe.x < self.x): return 1
+		if(foe.x+foe.width > self.x+self.width and foe.x < self.x+self.width): return 2
+		if(foe.x+foe.width < self.x): return -1
+		if(foe.x > self.x + self.width): return -2
+		if(foe.x+foe.width < self.x+self.width and foe.x > self.x): return -3
+	def die(self):
+		#print "dead"
+		self.active = False
+
+class Bullet(MoveableObject):
+	def __init__(self, x, y, img, dir):
+		MoveableObject.__init__(self, x, y, img)
+		self.dir = dir
+		#self.active = True
+		self.speed = 5
+	def move(self):
+		# dir 0 for up, anything else for down
+		if(self.dir == UP): self.y -= self.speed
+		else: self.y += self.speed
+		self.updatepos()
+
+def collide(spr1, spr2):
+	return pygame.sprite.collide_rect(spr1, spr2)
