75197030f4335175dde7e763ba0b265fbc6a93f2
tnibert <docz2a@gmail.com>
2018-11-04 18:47:59 +1100

    Initial basic successful windows build

--- buildforwindows.bat
+++ buildforwindows.bat
@@ -1,9 +1,4 @@
-@echo off
-set WORKSPACE=%~dp0
-pushd "%WORKSPACE%"
-
-set output_folder=%WORKSPACE%\build\
-
-call python -m PyInstaller killthealiens.py -y --onefile --log-level ERROR --clean --distpath %output_folder%
-
-popd
+rem tested with 32 bit gcc/mingw 7.2.0/5.0.3 and 32 bit python 2.7
+rem your paths to python and mingw may vary
+set PATH=C:\Program Files (x86)\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin;%PATH%
+call C:\Python27-32\python.exe -m nuitka --follow-imports --standalone killthealiens.py
