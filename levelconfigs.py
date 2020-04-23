8a655864e019e33bcf30b05a98e5b981e8bdbdaa
Tim Nibert <docz2a@gmail.com>
2020-04-23 19:27:39 +1000

    reswapped levels

--- levelconfigs.py
+++ levelconfigs.py
@@ -10,19 +10,19 @@ the various levels.
 # todo: add boss health to this config
 level_configs = [
     {
-        "background": pygame.image.load("assets/simple_level/map.png"),
-        "bg_music_fname": "assets/spectre.mp3",
-        "boss_image": pygame.image.load("assets/simple_level/invader.png"),
-        "enemy_image": pygame.image.load("assets/simple_level/saucer.png"),
-        "boss_class": boss.InvaderBossBehave,
-        "start_text": "LEVEL 1"
-    },
-    {
         "background": pygame.image.load("assets/magykal_level/map.png"),
         "bg_music_fname": "assets/spectre.mp3",
         "boss_image": pygame.image.load("assets/magykal_level/boss.png"),
         "enemy_image": pygame.image.load("assets/magykal_level/saucer.png"),
         "boss_class": boss.MagykalBossBehave,
+        "start_text": "LEVEL 1"
+    },
+    {
+        "background": pygame.image.load("assets/simple_level/map.png"),
+        "bg_music_fname": "assets/spectre.mp3",
+        "boss_image": pygame.image.load("assets/simple_level/invader.png"),
+        "enemy_image": pygame.image.load("assets/simple_level/saucer.png"),
+        "boss_class": boss.InvaderBossBehave,
         "start_text": "LEVEL 2"
     }
 ]
