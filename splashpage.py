332f7f50f3932ebde12a4727e8d9b43e7ddcc3d8
Tim Nibert <docz2a@gmail.com>
2020-04-24 12:06:43 +1000

    added docstrings to splash page

--- splashpage.py
+++ splashpage.py
@@ -5,7 +5,18 @@ from endgamesignal import EndLevel
 
 
 class SplashPage(Strategy):
+    """
+    A level strategy in which we display a spash page until
+    a specified key is pressed.
+    """
+
     def __init__(self, scene, inputqueue, image, trigger_key):
+        """
+        :param scene: Scene object
+        :param inputqueue: Queue object for the input
+        :param image: a static image to display
+        :param trigger_key: the key the user must type to continue
+        """
         super().__init__(scene)
         self.inputqueue = inputqueue
         self.trigger_key = trigger_key
